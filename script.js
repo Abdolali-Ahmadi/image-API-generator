cb0365e2a0c43c23e1f8ca26d95c1b231f97b067
rahimi <rahimi.engineer.1378@gmail.com>
2023-04-28 22:16:27 +0430

    add ui

---
+++ script.js
@@ -0,0 +1,3 @@
+let apiKey = 'tsqjORZl6xIKThgQVNB-jAsQvKNe1PthE-AlOVir-Yk'
+// let secretKey = 'GgeZ8j1xvYje9sRyyPynqO1TlgSWapxG1khuWaTASLs'
+let urlBase = 'https://api.unsplash.com/search/photos/?query='+Text.value+'/?client_id=YOUR_ACCESS_KEY'
